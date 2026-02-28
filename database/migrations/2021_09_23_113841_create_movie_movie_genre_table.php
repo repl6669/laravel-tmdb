c26e5d223a8ffd9ed43f1f4b3c3e89aedcb52df7
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2026-02-28 12:13:32 +0100

    Restore migration files from main branch

--- database/migrations/2021_09_23_113841_create_movie_movie_genre_table.php
+++ database/migrations/2021_09_23_113841_create_movie_movie_genre_table.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD
-return new class extends Migration
-=======
-return new class extends Migration
->>>>>>> main
+return new class() extends Migration
 {
     public function up(): void
     {
